3fbd92a642421fb4965904e10bb605e618608e4d
emrecncelik <emrecncelik@gmail.com>
2025-03-10 19:04:50 +0300

    add bash script to run experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,2 +1,16 @@
-python run_experiment.py --config configs/experiment_mlm.yaml
-python run_experiment.py --config configs/experiment_mlm_nat.yaml
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <model_type>"
+  exit 1
+fi
+
+model_type="$1"
+configs=($(ls -1 configs | sort))
+
+for cfg in "${configs[@]}"; do
+  if [[ $cfg == $model_type* ]]; then
+    echo "Current config: $cfg"
+    python run_experiment.py --config configs/$cfg
+  fi
+done
